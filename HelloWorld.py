b3466e712be4634b5ed26cf06598fc30bd571cdb
lynn2610 <lynn_mg_study@163.com>
2018-10-19 14:48:24 +0800

    add helloworld

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/19 14:48
+# @Author  : Lynn
+# @Site    : 
+# @File    : HelloWorld.py
+# @Software: PyCharm
+print('hello world')
